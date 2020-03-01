61b812b12969d8faff90bc29b5603d943f031c76
Nick Spreitzer <nick@200-ok.com>
2020-03-01 12:31:01 -0600

    fixup! Refactor solution into 3 projects

---
+++ src/JournalCli.Library/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("JournalCli.Tests")]
+[assembly: InternalsVisibleTo("JournalCli.Pwsh")]
+[assembly: InternalsVisibleTo("JournalCli.GenericCli")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
